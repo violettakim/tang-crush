5e5d76f2612dbb3975b1e850f36f5b261f578181
violettakim <88869240+violettakim@users.noreply.github.com>
2021-08-13 10:32:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hey bae!',
+    introDesc: `Có vài điều mình muốn hỏi hãy nhớ là phải trả lời thật lòng nhaaaaaaaaaaaa`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Có phỉa mình là người bạn yêu nhất trên đời phải khummmmm😙',
+    desc: 'Bạn mà thoát ra thì bạn là con mình:)))))_ ',
+    btnYes: 'đúng òi iu cậu nhìu lắmmmm <33',
+    btnNo: 'Không, mơ à :3',
+    question:'whyyyyy <3',
+    btnReply: 'tại vì bạn xinh đẹp và đáng yêu lắm á <3',
+    reply: 'tell me <33333333',
+    mess: 'i knew that 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'hết dịch mình cùng đi ăn bún đậu nhé',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: https://www.facebook.com/profile.php?id=100065500240341
 }
